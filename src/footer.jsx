75c9762d3afb81f840a0e9e2dd25f7ed83cc270d
honeylemon <char22079@gmail.com>
2024-01-10 23:09:30 +0900

    license削除

--- src/footer.jsx
+++ src/footer.jsx
@@ -4,9 +4,7 @@ export default function Footer() {
         <div className="content has-text-centered">
           <p>日本大学文理学部情報科学科 Webプログラミング 課題制作</p>
           <p>5422079 須貝彩莉紗</p>
-          <p>
-            <a href="./WebShareAPILicense.txt">This is "Web Share API License"</a>
-          </p>
+          
         </div>
       </footer>
     );
